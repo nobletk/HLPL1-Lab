7bfaddae8628cbe0f52783245e733a864b57a7b6
nobletk <80173312+nobletk@users.noreply.github.com>
2021-04-08 15:01:54 +0200

    Add files via upload

---
+++ Ch10/Drill1/Ch10_Drill1.cpp
@@ -0,0 +1,17 @@
+// Ch10_Drill1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "std_lib_facilities.h"
+struct Point
+{
+	double x, y;
+};
+
+vector<Point>v_points;
+
+int main()
+{
+	char ch;
+	cin >> ch;
+	return 0;
+}
